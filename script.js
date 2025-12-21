8e6fea59027ef3c23b010f30230d4d15954f7765
Ranielli <ranielliakira@gmail.com>
2025-12-20 22:52:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const gravity = 0.8;
 const zoom = 1.6; 
 const mapWidth = 7000; 
 let cameraX = 0;
-let cameraY = 120; // Valor fixo para garantir que o chão apareça com o zoom
+let cameraY = 0;
 let gameState = 'menu';
 let isPaused = false;
 let isMuted = false;
@@ -32,16 +32,17 @@ const player = {
 let enemies = [];
 function initEnemies() {
     enemies = [
-        { type: 'Green_Slime', x: 800, y: 320, hp: 1, damage: 1, speed: 1.2, range: 200, attackType: 'melee', attackRange: 50, state: 'patrol', walkFrames: 8, attackFrames: 4, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
-        { type: 'Blue_Slime', x: 2500, y: 320, hp: 1, damage: 1, speed: 1.8, range: 250, attackType: 'melee', attackRange: 50, state: 'jumping', walkFrames: 8, attackFrames: 4, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
-        { type: 'Red_Slime', x: 4000, y: 320, hp: 1, damage: 1, speed: 2.5, range: 450, attackType: 'melee', attackRange: 50, state: 'chase', walkFrames: 8, attackFrames: 4, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
-        { type: 'Enchantress', x: 6600, y: 300, hp: 3, damage: 1, speed: 2, range: 400, attackType: 'melee', attackRange: 100, walkFrames: 8, attackFrames: 6, deadFrames: 5, currentFrame: 0, frameTimer: 0, frameInterval: 4  }
+        { type: 'Green_Slime', x: 800, y: 320, hp: 1, damage: 1, speed: 1.2, range: 200, attackType: 'melee', attackRange: 50, state: 'patrol', walkFrames: 8, attackFrames: 4, hurtFrames: 6, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
+        { type: 'Blue_Slime', x: 2500, y: 320, hp: 1, damage: 1, speed: 1.8, range: 250, attackType: 'melee', attackRange: 50, state: 'jumping', walkFrames: 8, attackFrames: 4, hurtFrames: 6, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
+        { type: 'Red_Slime', x: 4000, y: 320, hp: 1, damage: 1, speed: 2.5, range: 450, attackType: 'melee', attackRange: 50, state: 'chase', walkFrames: 8, attackFrames: 4, hurtFrames: 6, deadFrames: 3, currentFrame: 0, frameTimer: 0, frameInterval: 4 },
+        { type: 'Enchantress', x: 6600, y: 300, hp: 3, damage: 1, speed: 2, range: 400, attackType: 'melee', attackRange: 100, walkFrames: 8, attackFrames: 6, hurtFrames: 2, hurtFrames: 6, deadFrames: 5, currentFrame: 0, frameTimer: 0, frameInterval: 4  }
     ];
 
     enemies.forEach(en => {
         en.imgWalk = new Image(); en.imgWalk.src = `assets/${en.type}/Walk.png`;
         en.imgAttack = new Image(); en.imgAttack.src = `assets/${en.type}/Attack_1.png`;
         en.imgDead = new Image(); en.imgDead.src = `assets/${en.type}/Dead.png`;
+        en.imgDead = new Image(); en.imgHurt.src = `assets/${en.type}/Hurt.png`;
         en.width = 100; en.height = 100;
         en.currentFrame = 0; en.frameTimer = 0; en.frameInterval = 8;
         en.state = 'patrol'; en.facing = 'left';
@@ -83,6 +84,8 @@ window.escolherPersonagem = function(genero) {
     player.imgJump.src = `assets/${folder}/Jump.png`;
     player.imgDead.src = `assets/${folder}/Dead.png`;
     player.imgAttack.src = `assets/${folder}/Attack_1.png`;
+    player.imgHurt = new Image();
+    player.imgHurt.src = `assets/${folder}/Hurt.png`;
 
     gameState = 'playing';
     initEnemies();
@@ -250,6 +253,7 @@ window.addEventListener('keyup', (e) => {
     if(k === 'a') window.mover('left', false);
     if(k === 'd') window.mover('right', false);
 });
+
 
 
 
